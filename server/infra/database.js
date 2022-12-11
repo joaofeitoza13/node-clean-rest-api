fcca142799221f95ad6210dfc4033d59a2c02fb8
João Bisneto <joaofeitoza.13@gmail.com>
2022-12-10 21:59:42 -0300

    Fixing port access. Adding mensage for debugging purposes.

--- server/infra/database.js
+++ server/infra/database.js
@@ -4,8 +4,11 @@ const db = pgp({
   user: 'postgres',
   password: 'Shadow)491',
   host: 'localhost',
-  port: 3333,
+  port: 3000,
   database: 'blog'
 })
+
+console.log("Connected to DB.")
+console.log("----------------")
 
 module.exports = db
