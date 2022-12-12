d9b2e20d3596c0fbecd1f3ed6c7a54c7af688ea8
João Bisneto <joaofeitoza.13@gmail.com>
2022-12-12 12:07:33 -0300

    Enables parsing of the incoming request.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express()
 
-
 console.log("---------------")
 console.log("Server Running.")
 console.log("---------------")
+
+app.use(express.json())
 
 app.use('/', require('./route/postsRoute'))
 
