f4766faf7f0e22eb6b303972e2054aacc772d4c8
João Bisneto <joaofeitoza.13@gmail.com>
2022-12-12 12:15:04 -0300

    Removing comments.

--- server/route/postsRoute.js
+++ server/route/postsRoute.js
@@ -5,10 +5,6 @@ const postsService = require('../service/postsService')
 
 
 router.get('/posts/', async function (req, res) {
-  console.log("Routes Layer.")
-  console.log("-")
-  console.log("Requesting all posts.")
-  console.log("---------------------")
 
   const posts = await postsService.getPosts()
 
@@ -28,11 +24,6 @@ router.put('/posts/:id', async function (req, res) {
 })
 
 router.delete('/posts/:id', async function (req, res) {
-  console.log("Routes Layer.")
-  console.log("-")
-  console.log("Requesting deletion of all posts.")
-  console.log("---------------------------------")
-
   await postsService.deletePosts()
   console.log("All posts deleted.")
 })
