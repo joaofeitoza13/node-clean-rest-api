529ecbbed8bf66a752cb77af63b082cca77ad6e2
João Bisneto <joaofeitoza.13@gmail.com>
2022-12-12 12:12:55 -0300

    Removing comments and formatting savePost.

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -3,17 +3,11 @@ const database = require('../infra/database');
 
 
 exports.getPosts = function () {
-  console.log("Data Layer.")
-  console.log("-")
-  console.log("Running getPosts.")
-  console.log("-----------------")
-
   return database.query('SELECT * FROM blog.post')
 }
 
 exports.savePost = function (post) {
-  return database.one('INSERT INTO blog.post (title, content) VALUES ($1, $2) returning *', 
-  [post.title, post.content])
+  return database.one('INSERT INTO blog.post (title, content) VALUES ($1, $2) returning *', [post.title, post.content])
 }
 
 exports.deletePosts = function () {
