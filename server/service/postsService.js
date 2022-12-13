9579ece195bcbd73e210a7968cd35ee491cc86bc
João Bisneto <joaofeitoza.13@gmail.com>
2022-12-13 01:02:42 -0300

    Adding error handling and validation to savePost.

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -2,19 +2,26 @@ const postsData = require('../data/postsData')
 
 
 
-exports.getPost = function (id) {
-  return postsData.getPost(id)
+exports.getPost = async function (id) {
+  const post = await postsData.getPost(id)
+  if(!post) throw new Error('Post not found.')
+  
+  return post
 }
 
 exports.getPosts = function () {
   return postsData.getPosts()
 }
 
-exports.savePost = function (post) {
+exports.savePost = async function (post) {
+  const existingPost = await postsData.getPostByTitle(post.title)
+  if(existingPost) throw new Error('Post already exists.')
+
   return postsData.savePost(post)
 }
 
-exports.updatePost = function (id, post) {
+exports.updatePost = async function (id, post) {
+  await exports.getPost(id)
   return postsData.updatePost(id, post)
 }
 
